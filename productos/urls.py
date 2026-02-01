666496c7c38c9b2a2d744c7dfbe44be445c662fb
lvazgaries <lvazgar131@fp.iesromerovargas.com>
2026-02-01 12:37:14 +0100

    añadido el class view

--- productos/urls.py
+++ productos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import ProductoListView, ProductoCreateView, ProductoUpdateView, DeleteProductoView
 
 urlpatterns = [
-    path('', views.producto_list, name='producto_list'),
-    path('nuevo/', views.producto_create, name='producto_create'),
-    path('<int:pk>/editar/', views.producto_update, name='producto_update'),
-    path('<int:pk>/borrar/', views.producto_delete, name='producto_delete'),
+    path('', ProductoListView.as_view(), name='producto_list'),
+    path('nuevo/',ProductoCreateView.as_view(),name='producto_create'),
+    path('editar/<int:pk>',ProductoUpdateView.as_view(),name='producto_update'),
+    path('eliminar/<int:pk>',DeleteProductoView.as_view(),name='producto_delete'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,35 +1,58 @@
-from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .forms import ProductoForm
 from .models import Producto
 
 # Create your views here.
-def producto_list(request):
-    productos = Producto.objects.all().order_by('nombre')
-    return render (request, "productos/producto_list.html",{"productos":productos})
 
-def producto_create(request):
-    form = ProductoForm(request.POST or None)
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        messages.success(request, "Producto creado correctamente.")
-        return redirect("producto_list")
-    return render (request, "productos/producto_form.html", {"form": form, "modo":"crear"})
+class ProductoListView (ListView):
+    model = Producto
+    template_name = "productos/producto_list.html"
+    context_object_name = "productos"
+    ordering = ["nombre"]
 
-def producto_update(request,pk):
-    producto = get_object_or_404(Producto,pk = pk)
-    form = ProductoForm(request.POST or None,instance=producto)
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        messages.success(request, "Producto actualizado correctamente.")
-        return redirect("producto_list")
-    return render (request, "productos/producto_form.html", {"form": form, "modo":"editar","producto":producto})
 
-def producto_delete(request,pk):
-    producto =  get_object_or_404(Producto,pk = pk)
-    if request.method == "POST":
-        producto.delete()
-        messages.success(request,"Producto eliminado correctamente.")
-        return redirect("producto_list")
-    return render (request, "productos/producto_confirm_delete.html", {"producto":producto})
+class ProductoCreateView (CreateView):
+    model = Producto
+    form_class = ProductoForm
+    template_name = "productos/producto_form.html"
+    success_url = reverse_lazy("producto_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Producto creado correctamente.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["modo"] = "crear"
+        return context
+
+
+
+
+class ProductoUpdateView (UpdateView):
+    model = Producto
+    form_class = ProductoForm
+    template_name = "productos/producto_form.html"
+    success_url = reverse_lazy("producto_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Producto actualizado correctamente.")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["modo"] = "editar"
+        return context
+
+
+class DeleteProductoView(DeleteView):
+    model = Producto
+    template_name = "productos/producto_confirm_delete.html"
+    success_url = reverse_lazy("producto_list")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Producto eliminado correctamente.")
+        return super().delete(request, *args, **kwargs)
